a4eda03ca849b21656fd10a84f9841c9f5e0a85a
sweatySpaghett1 <83618867+sweatySpaghett1@users.noreply.github.com>
2021-05-03 19:50:20 -0400

    UPDATE
    Now with mostly functional sound sensor

--- project_D.ino
+++ project_D.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
- 
+
 Servo theVessel;
 
 void setup(){
@@ -7,8 +7,7 @@ void setup(){
   pinMode(9, OUTPUT);
   pinMode(A0, INPUT);
   Serial.begin(9600);
-  pinMode(8, OUTPUT);
-  //digitalWrite(8,HIGH);
+  pinMode(8, INPUT);
   theVessel.attach(9);
   pinMode(2,INPUT);
   
@@ -24,7 +23,8 @@ long buttonTimer = 0;
 long longPressTime = 250;
 
 void loop(){
-  
+  //A very long, convoluted, and messy conditional statement allowing dual function of the button.
+  //It's like a circus funhouse for my manual boolean, twisted every which way.
   if (digitalRead(buttonPin) == HIGH) {
 
     if (buttonActive == false) {
@@ -70,32 +70,22 @@ void loop(){
     }
 
   }
+  //Actully only works when manual is false, I don't know why
   if(manual == true){
     //I don't know why it does this
     Serial.println("Manual off");
     if(analogRead(A0) > 800 && isLocked == false){
      doLock();
     }
-    
-  
-    
-    
-    
+    if(digitalRead(8) == HIGH && isLocked == false){
+      doLock();
+    }
     
     
+   
   }
-//  if(manual){
-//    Serial.println("This is manual mode");
-//  }
-//  if(isLocked){
-//    Serial.println("This is locked");
-//  }
-//  if(!isLocked){
-//    Serial.println("This is not locked");
-//    
-// }
-//  Serial.println(analogRead(A0));
-//  delay(100);
+Serial.println(digitalRead(8));
+Serial.println(analogRead(A0));
   
   
   
