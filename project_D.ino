98e4ea3fa3f0cac0edce46c02b6ea9e1786fd676
sweatySpaghett1 <83618867+sweatySpaghett1@users.noreply.github.com>
2021-05-04 18:44:45 -0400

    Add files via upload
    Updated to 5/4/21

--- project_D.ino
+++ project_D.ino
@@ -10,6 +10,7 @@ void setup(){
   pinMode(8, INPUT);
   theVessel.attach(9);
   pinMode(2,INPUT);
+  theVessel.write(0);
   
 }
 
@@ -26,30 +27,19 @@ void loop(){
   //A very long, convoluted, and messy conditional statement allowing dual function of the button.
   //It's like a circus funhouse for my manual boolean, twisted every which way.
   if (digitalRead(buttonPin) == HIGH) {
-
     if (buttonActive == false) {
-
       buttonActive = true;
       buttonTimer = millis();
-
     }
-
     if ((millis() - buttonTimer > longPressTime) && (longPressActive == false)) {
-
       longPressActive = true;
       manual = !manual;
       Serial.println("Switching sides");
-
     }
-
   } else {
-
     if (buttonActive == true) {
-
       if (longPressActive == true) {
-
         longPressActive = false;
-
       } else {
         if(!manual){
           if(isLocked){
@@ -59,16 +49,9 @@ void loop(){
                 doLock();
               }
         }
-                
-              
-        
-
       }
-
       buttonActive = false;
-
     }
-
   }
   //Actully only works when manual is false, I don't know why
   if(manual == true){
@@ -79,23 +62,17 @@ void loop(){
     }
     if(digitalRead(8) == HIGH && isLocked == false){
       doLock();
-    }
-    
-    
-   
+    }  
   }
-Serial.println(digitalRead(8));
-Serial.println(analogRead(A0));
-  
-  
-  
 }
 
 
 //Sets the boolean to true, then rotate it around
 void doLock(){
   isLocked = true;
-  theVessel.write(180);
+  theVessel.write(179);
+  //delay(1000);
+  //theVessel.write(90);
   Serial.println("Did lock");
 }
 
@@ -106,4 +83,3 @@ void doUnLock(){
   theVessel.write(0);
   Serial.println("Did unlock");
 }
-//Changes the isOut boolean from true or to false;
